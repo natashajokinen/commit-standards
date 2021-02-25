137eb50acb16e1b5982861e2f2624b9191de2232
natashajokinen <76977006+natashajokinen@users.noreply.github.com>
2021-02-24 22:31:45 -0500

    feat(config): accept a commit-standards.yml config file and better failure msg

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # commit-standards
 
-A Github App that verifies all commits in a PR follow the configured standards.
+A Github App that verifies all commits in a PR follow the configured standards. Your answer to "my repo follows the community standard except..." or for easily enforcing your own standard.
 
 ## Configuration
 
 The standards enforced can be configured by the repo. Just add a `commit-standards.yml` file to the .`github` directory of the repo. 
 
 ```
-# Each line of the file can contain a regex expression that a commit must match.
-# Lines in a commit message that don't have a corresponding regex expression in
+# Each line of the file can contain a regex pattern that a commit must match.
+# Lines in a commit message that don't have a corresponding regex pattern in
 # the `commit-standards.yml` are not matched.
+# A line that should only be a new line should be indicated with an empty string `''`
 regexArray:
-  - /^(?:feat|fix|docs|style|refactor|perf|test|chore|revert|demo|deprecate)(?:\(.+\))?: [^A-Z ].+[^\.]$/
-  - /\n/
-  - /description/
+  - '^(?:feat|fix|docs|style|refactor|perf|test|chore|revert|demo|deprecate)(?:\(.+\))?: [^A-Z ].+[^\.]$'
+  - ''
+  - 'description'
 ```
 
 ```
@@ -24,7 +25,7 @@ commitStandardsDocumentation: 'https://github.com/natashajokinen/commit-standard
 The default configured standards are the same as the [conventional commits definition](https://conventionalcommits.org/) with additional requirements that:
 * the description is not capitalized
 * the description does not end with a `.`
-* the allowed types are listed below. Descriptions of each type are the same as [conventional commit types](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) or self-explanitory
+* the allowed types are listed below. Descriptions of each type are the same as [conventional commit types](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) or self-explanatory
   * `feat`
   * `fix`
   * `docs`
@@ -36,6 +37,10 @@ The default configured standards are the same as the [conventional commits defin
   * `revert`
   * `demo`
   * `deprecate`
+
+## Alternatives
+
+If you'd like to follow the convetional commits definition as it is written [zeke/semantic-pull-requests](https://github.com/zeke/semantic-pull-requests) does just that.
 
 ## Contributing
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const DEFAULT_OPTIONS = {
-  regexArray: [/^(?:feat|fix|docs|style|refactor|perf|test|chore|revert|demo|deprecate)(?:\(.+\))?: [^A-Z ].+[^\.]$/],
+  regexArray: ['^(?:feat|fix|docs|style|refactor|perf|test|chore|revert|demo|deprecate)(?:\(.+\))?: [^A-Z ].+[^\.]$'],
   commitStandardsDocumentation: 'https://github.com/natashajokinen/commit-standards',
 };
 
@@ -10,22 +10,21 @@ const DEFAULT_OPTIONS = {
  */
 module.exports = (app) => {
   app.on(['pull_request.opened', 'pull_request.synchronize'], async (context) => {
-      //TODO write getConfig
-      // const userConfig = await getConfig(context, 'commit-standards.yml', {});
-      // const {regexArray} = Object.assign({}, DEFAULT_OPTIONS, userConfig);
-      const {commitStandardsDocumentation, regexArray} = Object.assign({}, DEFAULT_OPTIONS);
+      const userConfig = await context.config('commit-standards.yml');
+      let {commitStandardsDocumentation, regexArray} = Object.assign({}, DEFAULT_OPTIONS, userConfig);
+      regexArray = regexArray.map((regex)=> new RegExp(regex));
 
       const pullRequestObj = context.repo({
         pull_number: context.payload.pull_request.number
       });
       const commitInfo = await context.octokit.pulls.listCommits(pullRequestObj);
       const commits = commitInfo.data;
-      const areCommitsStandard = verifyCommits(commits, regexArray);
+      const {areCommitsStandard, invalidCommit = ''} = verifyCommits(commits, regexArray);
 
       const status = {
         sha: context.payload.pull_request.head.sha,
         state: areCommitsStandard ? 'success' : 'failure',
-        description: areCommitsStandard ? 'commits are following standards' : 'every commit needs to follow commit standards',
+        description: areCommitsStandard ? 'Commits are following standards' : `Commit ${invalidCommit} doesn't follow commit standards.`,
         context: 'Following Commit Standards',
         target_url: commitStandardsDocumentation,
       };
@@ -36,7 +35,18 @@ module.exports = (app) => {
 };
 
 function verifyCommits(commits, regexArray) {
-  return commits.map(fullCommit => fullCommit.commit).every(commit => matchesStandards(commit.message, regexArray));
+  let invalidCommit;
+  const areCommitsStandard = commits.map(fullCommit => fullCommit.commit).every((commit, index) => {
+    const matches = matchesStandards(commit.message, regexArray);
+    if (!matches) {
+      invalidCommit = commits[index].sha.slice(0,7);
+    };
+    return matches;
+  });
+  return {
+    areCommitsStandard,
+    invalidCommit,
+  };
 };
 
 function matchesStandards(message, regexArray) {
